f84f8dd847015cdb33829d94e2eca5b2116f9a31
Carina Sweet <carina.sweet06@gmail.com>
2021-06-13 15:39:08 -0600

    Add multiple alphabets per shift

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,18 +1,56 @@
 require 'date'
 
 class Enigma
-  attr_reader :char_set
+  attr_reader :alphabet
 
   def initialize
-    @char_set = ("a".."z").to_a << " "
+    @alphabet = ("a".."z").to_a << " "
   end
 
-  def encrypt(message, key = KeyGenerator.random_key, date = Date.todays_date)
+  def encrypt(message, key = KeyGenerator.random_key, date = Date::today)
     hash = Hash.new
   end
 
-  def decrypt(encrypted_message, key, date = Date.todays_date)
+  def decrypt(encrypted_message, key, date = Date::today)
     hash = Hash.new
+  end
+
+  def message_with_alphabet_index(message)
+    message_with_alphabet_index = []
+    message.each_char do |char|
+      message_with_alphabet_index << @alphabet.index(char)
+    end
+    message_with_alphabet_index
+  end
+
+  def alphabet_a
+    @alphabet.rotate(3)
+  end
+
+  def alphabet_b
+    @alphabet.rotate(27)
+  end
+
+  def alphabet_c
+    @alphabet.rotate(73)
+  end
+
+  def alphabet_d
+    @alphabet.rotate(20)
+  end
+
+  def encryped_string
+    message_with_alphabet_index.map.with_index do |number, index|
+      if (index + 1) % 4 == 1
+        alphabet_a[number]
+      elsif (index +1) % 4 == 2
+        alphabet_b[number]
+      elsif (index + 1) % 4 == 3
+        alphabet_c[number]
+      elsif (index + 1) % 4 == 0
+        alphabet_d[number]
+      end
+    end.join
   end
 
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 require 'date'
 require './lib/key_generator'
-require './lib/date'
 require './lib/shifts'
 require './lib/enigma'
 
@@ -21,4 +20,45 @@ RSpec.describe Enigma do
   xit 'can decrypt a message' do
     expect(@enigma.encrypt("keder ohulw")).to eq("hello world")
   end
+
+  it 'can assign new character' do
+    message = "hello world"
+
+    expected = [7, 4, 11, 11, 14, 26, 22, 14, 17, 11, 3]
+
+    expect(@enigma.message_with_alphabet_index(message)).to eq(expected)
+  end
+
+  it 'creates alphabet for shift A' do
+    expected = ["d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "a", "b", "c"]
+
+    expect(@enigma.alphabet_a).to eq(expected)
+  end
+
+  it 'creates alphabet for shift B' do
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+    expect(@enigma.alphabet_b).to eq(expected)
+  end
+
+  it 'creates alphabet for shift C' do
+    expected = ["t", "u", "v", "w", "x", "y", "z", " ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s"]
+
+    expect(@enigma.alphabet_c).to eq(expected)
+
+  end
+
+  it 'creates alphabet for shift D' do
+    expected = ["u", "v", "w", "x", "y", "z", " ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t"]
+
+    expect(@enigma.alphabet_d).to eq(expected)
+  end
+
+  it 'encrypts message sklfjlsfk ' do
+    expected_1 = [7, 4, 11, 11, 14, 26, 22, 14, 17, 11, 3]
+    allow(@enigma).to receive(:message_with_alphabet_index).and_return(expected_1)
+
+    expect(@enigma.encryped_string).to eq("keder ohulw")
+  end
+
 end
