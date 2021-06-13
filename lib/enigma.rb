790e36ba535598b5e525fd1845b9c148e0a95b03
Carina Sweet <carina.sweet06@gmail.com>
2021-06-13 13:19:55 -0600

    Restart

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,74 +1,18 @@
+require 'date'
+
 class Enigma
-  attr_reader :key, :date
+  attr_reader :char_set
 
   def initialize
-    @key = key
-    @date = date
     @char_set = ("a".."z").to_a << " "
   end
 
   def encrypt(message, key = KeyGenerator.random_key, date = Date.todays_date)
-
+    hash = Hash.new
   end
 
   def decrypt(encrypted_message, key, date = Date.todays_date)
-
-  end
-
-  def message_char_index(message)
-    @a = [] @b = [] @c = [] @d = []
-    message.each_char.with_index do |char, index|
-     @a << char if index % 4 == 1
-     @b << char if index % 4 == 2
-     @c << char if index % 4 == 3
-     @d << char if index % 4 == 0
-    end
-  end
-
-  def character_rotate(message, shift_num)
-    key = Hash[@har_set.zip(char_set.rotate(number))]
-    message.each_char.reduce("") do |new_char, char|
-      new_char + key[char]
-    end
+    hash = Hash.new
   end
 
 end
-
-# def message_with_index(message)
-#   message_hash = {}
-#   message.each_char.with_index do |char, index|
-#    message_hash[index] = char.downcase
-#   end
-#   message_hash
-# end
-#
-# def message_with_shift
-#
-# end
-
-# def character_set_with_index
-#   char_set = ("a".."z").to_a << " "
-#   char_hash = {}
-#   char_set.each.with_index do |char, index|
-#     char_hash[index] = char
-#   end
-#   require "pry"; binding.pry
-#   char_hash
-# end
-
-# A = [0, 4, 8, 12, 16]
-# [0, 4, 8, 12, 16]
-# B = [1, 5, 9, 13, 17]
-# [1, 5, 9, 13, 17]
-# C = [2, 6, 10, 14, 18]
-# [2, 6, 10, 14, 18]
-# D = [3, 7, 11, 15, 19]
-# [3, 7, 11, 15, 19]
-# new_array.values_at(*A)
-# ["h", "o", "r", nil, nil]
-# new_array.values_at(*B)
-# ["e", " ", "l", nil, nil]
-# new_array.values_at(*C)
-# ["l", "w", "d", nil, nil]
-# new_array.values_at(*D)
-# ["l", "o", nil, nil, nil]
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 require 'date'
-require './lib/keys'
+require './lib/key_generator'
 require './lib/date'
 require './lib/shifts'
 require './lib/enigma'
@@ -14,32 +14,11 @@ RSpec.describe Enigma do
     expect(@enigma).to be_a(Enigma)
   end
 
-  xit 'assigns index to each character' do
-    message = "hello world"
-
-    expected = {0 => "h", 1 => "e", 2 => "l", 3 => "l", 4 => "o", 5 => " ", 6 => "w", 7 => "o", 8 => "r", 9 => "l", 10 => "d"}
-
-    expect(@enigma.message_with_index(message)).to eq(expected)
+  xit 'can encrypt a message' do
+    expect(@enigma.encrypt("hello world", "02715", "040895")).to eq("keder ohulw")
   end
 
-  xit 'it assigns each index to a shift letter' do
-    expected_1 = {0 => "h", 1 => "e", 2 => "l", 3 => "l", 4 => "o", 5 => " ", 6 => "w", 7 => "o", 8 => "r", 9 => "l", 10 => "d"}
-    expected_2 = {"A" => 3, "B" => 27, "C" => 73, "D" => 20}
-
-    allow(@enigma).to receive(:message_with_index).and_return(expected_1)
-    allow(@enigma).to receive(:final_shifts).and_return(expected_2)
-
-
-    expect(@enigma.message_with_shift).to eq(expected_2)
+  xit 'can decrypt a message' do
+    expect(@enigma.encrypt("keder ohulw")).to eq("hello world")
   end
-
-  xit 'assigns each character to index' do
-    expect(@enigma.character_set_with_index.length).to eq(27)
-  end
-
-  it 'assigns message to char index' do
-    message = "hello world"
-
-  end
-
 end
